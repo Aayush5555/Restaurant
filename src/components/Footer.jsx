62d6b723b781cdeaee4cb5487b14421a73ab8167
Aayush <97851608+Aayush5555@users.noreply.github.com>
2024-09-21 13:43:26 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">TIME_TRAVEL</div>
+          <div className="right">
+            <p>Hansi Gate Shop No.05 , BHIWANI, HARYANA</p>
+            <p>Open: 10:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Aayush Lakhera</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Time_travel.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
